7be9dfafe00cc78fe3db7444d4ce8c85624c3015
Dyary Raoof <dyray2020@gmail. om>
2023-07-22 13:20:38 +0300

    user sign up complete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,19 @@ class UsersController < ApplicationController
     def new
         @user = User.new
     end 
+
+    def create
+        @user = User.new(user_params) 
+        if @user.save 
+            flash[:notice] = "Succesfully signed up"
+            redirect_to articles_path
+        else
+            render :new,  status: :unprocessable_entity
+        end
+    end
+
+    private
+    def user_params 
+        params.require(:user).permit(:username, :password, :email)
+    end
 end
