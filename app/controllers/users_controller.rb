04cb468d92fe9b25487bbd8b147bd4b30df1694e
Dyary Raoof <dyray2020@gmail. om>
2023-07-22 13:35:26 +0300

    finish user edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,20 @@
 class UsersController < ApplicationController
+
+    def edit
+        @user = User.find(params[:id])
+    end
+
+    def update
+        @user = User.find(params[:id])
+        if @user.update(user_params)
+            flash[:notice] = "User updated successfully"
+            redirect_to articles_path
+        else
+            render :edit, status: :unprocessable_entity
+        end
+    end
+
+
     def new
         @user = User.new
     end 
